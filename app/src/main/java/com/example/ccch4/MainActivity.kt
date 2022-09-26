0eed54d3b6ac408b33e44a9130d99388e4d9b21b
trimanrah <broidn@gmail.com>
2022-09-26 23:43:24 +0700

    ok

--- app/src/main/java/com/example/ccch4/MainActivity.kt
+++ app/src/main/java/com/example/ccch4/MainActivity.kt
@@ -1,58 +1,130 @@
-package com.example.ccch4
-
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.ccch4.databinding.ActivityMainBinding
+import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.widget.ImageView
+import android.widget.TextView
+
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private val imgUserRock: ImageView by lazy { findViewById(R.id.img_user_rock) }
+    private val imgUserPaper: ImageView by lazy { findViewById(R.id.img_user_paper) }
+    private val imgUserScissors: ImageView by lazy { findViewById(R.id.img_user_scissors) }
+
+    private val imgComRock: ImageView by lazy { findViewById(R.id.img_com_rock) }
+    private val imgComPaper: ImageView by lazy { findViewById(R.id.img_com_paper) }
+    private val imgComScissors: ImageView by lazy { findViewById(R.id.img_com_scissors) }
+
+    private val tvResult: TextView by lazy { findViewById(R.id.tv_result) }
+    private val imgRefresh: ImageView by lazy { findViewById(R.id.img_refresh) }
+
+    private lateinit var userChoice: String
+    private lateinit var comChoice: String
+    private lateinit var result: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        imgUserRock.setOnClickListener {
+            userChoice = getString(R.string.rock)
+            toggleUserHand(it as ImageView)
+        }
 
-        setSupportActionBar(binding.toolbar)
+        imgUserPaper.setOnClickListener {
+            userChoice = getString(R.string.paper)
+            toggleUserHand(it as ImageView)
+        }
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        imgUserScissors.setOnClickListener {
+            userChoice = getString(R.string.scissors)
+            toggleUserHand(it as ImageView)
+        }
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        imgRefresh.setOnClickListener {
+            for (hand in listOf(
+                imgUserPaper,
+                imgUserRock,
+                imgUserScissors,
+                imgComPaper,
+                imgComRock,
+                imgComScissors
+            )) {
+                hand.alpha = 1F
+                hand.setBackgroundColor(Color.TRANSPARENT)
+                tvResult.setTextSize(TypedValue.COMPLEX_UNIT_SP, 60.0f)
+                tvResult.setBackgroundColor(Color.TRANSPARENT)
+                tvResult.setTextColor(Color.RED)
+                tvResult.setText(R.string.vs)
+            }
+        }
+
+    }
+
+    private fun toggleUserHand(imgUser: ImageView) {
+        Log.i(TAG, "User choose: $userChoice")
+        imgUser.setBackgroundResource(R.drawable.rounded_corner_frame)
+        for (hand in listOf(imgUserPaper, imgUserRock, imgUserScissors)) {
+            if (hand.tag != userChoice) {
+                hand.setBackgroundColor(Color.TRANSPARENT)
+            }
+        }
+        getComputerHand()
+    }
+
+    private fun drawComputerChoice() {
+        for (hand in listOf(imgComPaper, imgComRock, imgComScissors)) {
+            if (hand.tag != comChoice) {
+                hand.alpha = 0.6F
+                hand.setBackgroundColor(Color.TRANSPARENT)
+            } else {
+                hand.setBackgroundResource(R.drawable.rounded_corner_frame)
+                hand.alpha = 1F
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun getComputerHand() {
+        comChoice = listOf(
+            getString(R.string.rock),
+            getString(R.string.paper),
+            getString(R.string.scissors)
+        ).random()
+        Log.i(TAG, "Computer choose: $comChoice")
+        drawComputerChoice()
+        getWinner()
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun getWinner() {
+        val position = mapOf<String, Int>(
+            getString(R.string.scissors) to 1,
+            getString(R.string.rock) to 2,
+            getString(R.string.paper) to 3
+        )
+        val count = position[userChoice]!! - position[comChoice]!!
+        if (userChoice == comChoice) {
+            result = getString(R.string.draw)
+            tvResult.setTextSize(TypedValue.COMPLEX_UNIT_SP, 40.0f)
+            tvResult.setTextColor(Color.WHITE)
+            tvResult.setBackgroundColor(Color.BLUE)
+            tvResult.setText("DRAW")
+        } else if ((count == 1) || (count == -2)) {
+            result = getString(R.string.win)
+            tvResult.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25.0f)
+            tvResult.setTextColor(Color.WHITE)
+            tvResult.setBackgroundColor(Color.GREEN)
+            tvResult.text = "Pemain 1\nMENANG !"
+        } else {
+            result = getString(R.string.lose)
+            tvResult.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25.0f)
+            tvResult.setTextColor(Color.WHITE)
+            tvResult.setBackgroundColor(Color.GREEN)
+            tvResult.text = "Pemain 2\nMENANG !"
         }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        Log.i(TAG, "Final result: ${tvResult.text.toString().replace('\n',' ')}")
     }
 }
